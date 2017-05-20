c267c9dc07ad1b61ca1d2d0b36dbbf1f613bc80e
unknown <yadavrajkumar>
2017-05-20 17:15:45 +0530

    custom diretive scope example

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
 var app=angular.module("myApp",[]);
 app.controller("myController",["$scope",function($scope){
 
+    $scope.company="Techminds Pvt Ltd"
 
-$scope.message="hello Raj !!"
-$scope.companyName="Techminds Pvt Ltd"
-$scope.userDetails={
 
-         name: "Scott Desatnick",
-         address : "Boston United State"
+
+     $scope.userArray= [
+					{ employeeId : 101,
+					 employeeName: "Raj",
+					  salary :5000
+					},
+					{ companyId : "c100",
+					 companyName: "Techminds Learning Center",
+					},
+					{ customerId : 100,
+					 customerName: "Rajkumar",
+					  prize :3000
+					}
+
+				]
+	$scope.change  = function(){
+	$scope.company ="Techminds Solutions"
 }
 
 }])
-/*app.directive("myDirectivMy",function(){
 
-	return {
-		     template : "<h1>This is my custom directive </h1>"
-	}
-})*/
 app.directive("techmindsProjectAngularDirective",function(){
 
 	return {    
 
-		template : '<a href="#" class = "list-group">' +
-'<h4 class = "list-group-header"> {{companyName}} </h4>' +
-'<h4 class = "list-group-item"> {{userDetails.name}} </h4>' +
-'<h4 class = "list-group-text"> {{userDetails.address}} </h4>' +
-'</a>'
+templateUrl : 'customerDetails.html',
+scope : {
+
+companyName : '@',
+change :'&',
+user :'='
+
+
+}
+}
+
+/*compile: function(element,attributes){
+
+  console.log("In compile !!!")
+  console.log(element)
+  console.log(attributes)
+}
 		 
 
-	}
+	}*/
 })
