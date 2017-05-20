fbaf2deaad928aa06f00b8a703b6f656a5bb6e3a
unknown <yadavrajkumar>
2017-05-20 12:57:33 +0530

    other example of custom directive

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ app.controller("myController",["$scope",function($scope){
 
 
 $scope.message="hello Raj !!"
+$scope.companyName="Techminds Pvt Ltd"
+$scope.userDetails={
+
+         name: "Scott Desatnick",
+         address : "Boston United State"
+}
 
 }])
 /*app.directive("myDirectivMy",function(){
@@ -15,11 +21,12 @@ app.directive("techmindsProjectAngularDirective",function(){
 
 	return {    
 
-		    restrict : 'EACM',
-
- 
-		template : "<h1> This is my custom directive</h1>",
-		 replace : true
+		template : '<a href="#" class = "list-group">' +
+'<h4 class = "list-group-header"> {{companyName}} </h4>' +
+'<h4 class = "list-group-item"> {{userDetails.name}} </h4>' +
+'<h4 class = "list-group-text"> {{userDetails.address}} </h4>' +
+'</a>'
+		 
 
 	}
 })
